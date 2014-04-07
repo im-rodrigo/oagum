70b8ac9876f78ffa433ed154054a478749aff8f8
fourhoarsemen <rodrigopala91@gmail.com>
2014-04-07 14:41:19 -0700

    Filled up three normalized tables with data from previous, less reliable tables.

---
+++ oagum0.01projectfiles/oagum0.01/Controllers/Article_ArtAutRelController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using oagum0._01;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace oagum0._01.Controllers
+{
+    public class Article_ArtAutRelController : Controller
+    {
+        private ArticleAuthorRelationshipEntities db = new ArticleAuthorRelationshipEntities();
+
+        // GET: /Article_ArtAutRel/
+        public ActionResult Index()
+        {
+            return View(db.T_Article.ToList());
+        }
+
+        // GET: /Article_ArtAutRel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            T_Article t_article = db.T_Article.Find(id);
+            if (t_article == null)
+            {
+                return HttpNotFound();
+            }
+           
+            //var authors = 
+            //    from author in db.T_Author
+            //    join articleauthor in db.T_ArticleAuthor 
+            //    on author.ID equals articleauthor.AuthorId
+            //    select author 
+            //    wh
+
+            SqlConnection sqlConnection1 = new SqlConnection(db.Database.Connection.ConnectionString.ToString());
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "select x.Name from T_Author x join T_ArticleAuthor y on y.AuthorId = x.ID  where y.ArticleId = " + t_article.ID.ToString();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+
+            reader = cmd.ExecuteReader();
+            // Data is accessible through the DataReader object here.
+            StringBuilder authorstring = new StringBuilder(); 
+            while (reader.Read())
+            {
+                  ViewBag.Authors += reader.GetValue(0);
+                  ViewBag.Authors += "\r\n"; 
+            }
+            sqlConnection1.Close();
+
+            return View(t_article);
+        }
+
+        // GET: /Article_ArtAutRel/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //// POST: /Article_ArtAutRel/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include="ID,Title,PublishDate,Description,Source,InsertDate,CreatedBy")] T_Article t_article)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.T_Article.Add(t_article);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(t_article);
+        //}
+
+        //// GET: /Article_ArtAutRel/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    T_Article t_article = db.T_Article.Find(id);
+        //    if (t_article == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(t_article);
+        //}
+
+        //// POST: /Article_ArtAutRel/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include="ID,Title,PublishDate,Description,Source,InsertDate,CreatedBy")] T_Article t_article)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(t_article).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(t_article);
+        //}
+
+        //// GET: /Article_ArtAutRel/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    T_Article t_article = db.T_Article.Find(id);
+        //    if (t_article == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(t_article);
+        //}
+
+        //// POST: /Article_ArtAutRel/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    T_Article t_article = db.T_Article.Find(id);
+        //    db.T_Article.Remove(t_article);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ oagum0.01projectfiles/oagum0.01/extrafiles/SQLQuery1.sql
@@ -0,0 +1,66 @@
+﻿drop table T_ArticleSmallMeDa_02; 
+CREATE TABLE [dbo].[T_ArticleSmallMeDa_02] (
+    [ID]      INT            IDENTITY (1, 1) NOT NULL,
+	[KeyTitle] NVARCHAR (40) NOT NULL,
+    [Title]   NVARCHAR (400) NULL,
+    [PubDate] DATETIME       NULL,
+    [Link]    NVARCHAR (400) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+	CONSTRAINT [U_KeyTitle] UNIQUE NONCLUSTERED ([KeyTitle] ASC)
+);
+select * from T_ArticleVeryFullMeDa_02 
+where Title like '%An anthology of%';
+
+insert into dbo.T_ArticleSmallMeDa_02 (KeyTitle,Title,PubDate,Link) 
+select distinct SUBSTRING(x.Title,0,39),x.Title,x.PubDate,x.Relation 
+from T_ArticleVeryFullMeDa_02 x; 
+
+--CREATE TABLE [dbo].[T_Article] (
+--    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+--    [TypeId]      INT            NULL,
+--    [Title]       NVARCHAR (400) NULL,
+--    [PublishDate] DATETIME       NULL,
+--    [Description] TEXT           NULL,
+--    [Source]      NVARCHAR (400) NULL,
+--    [InsertDate]  DATETIME       NULL,
+--    [CreatedBy]   NVARCHAR (20)  NULL,
+--    PRIMARY KEY CLUSTERED ([ID] ASC)
+--);
+
+--insert into dbo.T_ArticleSmallMeDa (Title,PubDate,Link) select Title, PubDate, Relation from T_ArticleVeryFullMeDa;
+--alter table dbo.T_ArticleSmallMeDa drop column Author;
+--select * from T_ArticleSmallMeDa;
+--select * from T_AuthorSmallMeDa;
+--alter table dbo.T_AuthorSmallMeDa drop constraint UC_TA;
+--select distinct Author from T_AuthorSmallMeDa;
+
+--DROP TABLE T_AuthorSmallestMeDa; 
+
+--CREATE TABLE [dbo].[T_AuthorSmallestMeDa] (
+--    [Author]  NVARCHAR (50)  NULL,
+--    [ID]      INT            IDENTITY (1, 1) NOT NULL,
+--    PRIMARY KEY CLUSTERED ([ID] ASC)
+--);
+
+--select y.Title,y.Creator,x.Author from T_AuthorSmallMeDa x join T_ArticleVeryFullMeDa y on (x.Author = y.Creator) where x.Title <> y.Title;
+
+--delete from  T_AuthorSmallMeDa where Author = '';
+--select * from T_AuthorSmallMeDa where Author = '';
+
+--insert into T_AuthorSmallestMeDa (Author) select distinct Author from T_AuthorSmallMeDa_02;
+
+--select * from T_AuthorSmallestMeDa;
+
+--select x.Title, x.Author,z.ID 
+--from T_AuthorSmallMeDa x 
+--join T_ArticleSmallMeDa y
+--on x.Title = y.Title
+--join T_AuthorSmallestMeDa z
+--on x.Author = z.Author;
+--where x.Author like '%Odedede%';
+--select * from T_ArticleVeryFullMeDa_02 where Creator like '%Scaron%';
+--select count(*) from T_AuthorSmallestMeDa; 
+--select * from T_AuthorSmallestMeDa where Author like '%Odedede%';
+--select * from T_ArticleVeryFullMeDa; 
+--select SUBSTRING(Title,0,19) as ShortTitle from T_ArticleSmallMeDa;
+--select * from T_ArticleSmallMeDa;
